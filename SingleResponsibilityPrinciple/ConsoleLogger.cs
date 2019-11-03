760a27a89948837ce740079377b7d20164af665f
Bill Dixon <wdixon@css.edu>
2019-11-03 13:09:26 -0600

    Request  405 - "As a manager, I want to easily monitor the trading errors my traders make so I can provide them better training to avoid errors." Details: all logs should be sent to an XML file along with outputting to the console.

--- SingleResponsibilityPrinciple/ConsoleLogger.cs
+++ SingleResponsibilityPrinciple/ConsoleLogger.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-
+using System.IO;
 using SingleResponsibilityPrinciple.Contracts;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
 
 namespace SingleResponsibilityPrinciple
 {
@@ -9,6 +11,10 @@ namespace SingleResponsibilityPrinciple
         public void LogWarning(string message, params object[] args)
         {
             Console.WriteLine(string.Concat("WARN: ", message), args);
+            using (StreamWriter logfile = File.AppendText("log.xml"))
+            {
+                logfile.WriteLine("<log><message>" + message + "</message></log> ", args);
+            }
         }
 
         public void LogInfo(string message, params object[] args)
